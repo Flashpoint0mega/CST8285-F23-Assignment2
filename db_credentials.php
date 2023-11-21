1f40b0ac9a7233a716a8d626a7c7d2be07e23ced
Flashpoint0mega <flashpoint2230@gmail.com>
2023-11-21 18:29:26 -0500

    Update db_credentials.php

--- db_credentials.php
+++ db_credentials.php
@@ -2,7 +2,7 @@
 
 define("DB_SERVER", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "assignment2a");
+define("DB_PASS", "peter1234!");
+define("DB_NAME", "assignment02");
 
 ?>
